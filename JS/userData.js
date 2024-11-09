c6a0d037d9d9a47ee33537348123a1d8ad3eb3c8
NguyenPhiThienn <phithien1007@@gmail.com>
2024-11-09 19:19:30 +0700

    first

---
+++ JS/userData.js
@@ -0,0 +1,38 @@
+export const userData = [
+    {
+      id: 1,
+      userName: "NhuNhu",
+      fullName: "Hoàng Nguyễn Quỳnh Như",
+      passWord: "1111",
+      avatar: "./images/ABOUT_US/UserNhu.jpg",
+    },
+    {
+      id: 2,
+      userName: "QuynhNhu",
+      fullName: "Quỳnh Như",
+      passWord: "2222",
+      avatar: "./images/ABOUT_US/UserQNhu.jpg",
+    },
+    {
+      id: 3,
+      userName: "PhiXien",
+      fullName: "Phi Thiên Nguyễn",
+      passWord: "3333",
+      avatar: "./images/ABOUT_US/UserThien",
+    },
+    {
+      id: 4,
+      userName: "PhiThien",
+      fullName: "Nguyễn Phi Thiên",
+      passWord: "4444",
+      avatar: "./images/ABOUT_US/Member_imgThien.png",
+    },
+    {
+      id: 5,
+      userName: "ThienNguyen",
+      fullName: "Hoàng Phi Thiên",
+      passWord: "5555",
+      avatar: "./images/ABOUT_US/UserPThien.jpg",
+    },
+  ];
+  
